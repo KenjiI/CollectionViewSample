20aec0775667cb5503e35fbcc193798c2a6e6277
石井賢二 <espoir.ka@gmail.com>
2014-04-24 12:26:15 +0900

    add empty project

---
+++ CollectionViewSample/CollectionViewSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionViewSample
+//
+//  Created by 石井賢二 on 2014/04/24.
+//  Copyright (c) 2014年 石井賢二. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionViewSample/CollectionViewSampleTests/CollectionViewSampleTests.m
@@ -0,0 +1,34 @@
+//
+//  CollectionViewSampleTests.m
+//  CollectionViewSampleTests
+//
+//  Created by 石井賢二 on 2014/04/24.
+//  Copyright (c) 2014年 石井賢二. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CollectionViewSampleTests : XCTestCase
+
+@end
+
+@implementation CollectionViewSampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
